116129b62037579f59a8574818de3d4f70f36283
Hari kumar Gurudu <gharikumar2@gmail.com>
2017-08-29 15:33:46 +0200

    Update ConfigAnalysis.java
    added changes to predictions method

--- NFP/ConfigAnalysis.java
+++ NFP/ConfigAnalysis.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -17,8 +18,11 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 
 import de.ovgu.featureide.core.IFeatureProject;
+import de.ovgu.featureide.fm.core.base.FeatureUtils;
 import de.ovgu.featureide.fm.core.base.IFeature;
+import de.ovgu.featureide.fm.core.base.IFeatureModel;
 import de.ovgu.featureide.fm.core.configuration.Configuration;
+import de.ovgu.featureide.fm.core.configuration.DefaultFormat;
 import de.ovgu.featureide.fm.core.io.manager.ConfigurationManager;
 import de.ovgu.featureide.fm.core.io.manager.FileHandler;
 
@@ -39,22 +43,15 @@ public class ConfigAnalysisUtils {
 	 */
 	public static boolean[][] getConfigsMatrix(IFeatureProject featureProject, List<String> featureList) throws CoreException {
 		Collection<IFile> configs = new ArrayList<IFile>();
-		Collection<IFile> predicts = new ArrayList<IFile>();
-		
 		// check that they are config files
 		IFolder configsFolder = featureProject.getConfigFolder();
 		for (IResource res : configsFolder.members()) {
 			if (res instanceof IFile) {
 				if (((IFile) res).getName().endsWith(".config")) {
 					configs.add((IFile) res);
-				}else {
-					predicts.add((IFile) res);
 				}
 			}
-		}   
-		
-		computeFeaturePredictions(predicts);
-        
+		}    
 		boolean[][] matrix = new boolean[configs.size()][featureList.size()];
 		int iconf = 0;
 		for (IFile config : configs) {
@@ -74,10 +71,52 @@ public class ConfigAnalysisUtils {
 		return matrix;
 	}
 
-	public static Object[] computeFeaturePredictions(Collection<IFile> predicts){
+	public static Object[] computeFeaturePredictions(IFeatureProject featureProject) throws CoreException{
+		
 		List<String> PREDICTIONS = new ArrayList<String>();
 		List<String> FeatureProps = new ArrayList<String>();
 		List<String> FeatureValues = new ArrayList<String>();
+		Collection<IFile> predicts = new ArrayList<IFile>();
+		
+	    IFolder configsFolder = featureProject.getConfigFolder();
+		IFeatureModel featureModel = featureProject.getFeatureModel();
+	    Configuration configuration = new Configuration(featureModel);
+		FileHandler.load(Paths.get(featureProject.getCurrentConfiguration().getLocationURI()), configuration, new DefaultFormat());
+		List<IFeature> childF = new ArrayList<>();
+		List<IFeature> unselectedFeatures = configuration.getUndefinedSelectedFeatures();
+		System.out.println("abstract unselected array before filtering"+unselectedFeatures);
+		for(IFeature f: unselectedFeatures) {
+			if((FeatureUtils.isAbstract(f)) && (FeatureUtils.hasChildren(f))){
+				Iterable<IFeature> j = (Iterable<IFeature>) FeatureUtils.getChildren(f).iterator();
+				for (int i = 0; ((Iterator<IFeature>) j).hasNext(); i++)
+		        {
+					Object next = ((Iterator<IFeature>) j).next();
+					childF.add((IFeature) next);
+					
+		        }
+			}
+		}
+		System.out.println("abstract child array"+childF);
+		unselectedFeatures.removeAll(childF);
+			for(int i=0;i<childF.size();i++){
+				if(unselectedFeatures.contains(childF.get(i))){
+					unselectedFeatures.remove(i);
+					System.out.println(i);
+		
+				}
+		  }	
+		System.out.println("abstract undefined array after filtering"+unselectedFeatures);
+		System.out.println("unselected(inactive) Features from Feature Model "+configuration.getUnSelectedFeatures());
+		
+		
+		for (IResource res : configsFolder.members()) {
+			if (res instanceof IFile) {
+				if (((IFile) res).getName().endsWith(".txt")) {
+					predicts.add((IFile) res);
+				}
+			}
+		}   
+		
 		for (IFile predict : predicts) {
         	try {
 				java.io.InputStream is=predict.getContents();
@@ -102,16 +141,13 @@ public class ConfigAnalysisUtils {
 						FeatureValues.add(b1);
 					}
 				}	
-				System.out.println("File contents are "+ FeatureProps);
-				System.out.println("File contents are "+ FeatureValues);
+				//System.out.println("File contents are "+ FeatureProps);
+				//System.out.println("File contents are "+ FeatureValues);
 				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			} 
          
         }
 		return new Object[]{ FeatureProps, FeatureValues };
@@ -130,6 +166,7 @@ public class ConfigAnalysisUtils {
 		List<IFeature> coreFeatures = featureProject.getFeatureModel().getAnalyser().getCoreFeatures();
 		Collection<IFeature> hiddenFeatures = featureProject.getFeatureModel().getAnalyser().getHiddenFeatures();
 		for (IFeature coref : coreFeatures) {
+		     
 			featureList.remove(coref.getName());
 		}
 		for (IFeature hiddenf : hiddenFeatures) {
