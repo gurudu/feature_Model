f5c120dfa78160cbfefe7d6d4861fb26386bbe20
Hari kumar Gurudu <gharikumar2@gmail.com>
2017-09-02 21:57:20 +0200

    Update NFProps.java
    added new method

--- NFP/NFProps.java
+++ NFP/NFProps.java
@@ -3,7 +3,12 @@ package de.ovgu.featureide.visualisation;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
 
 import de.ovgu.featureide.core.IFeatureProject;
 import de.ovgu.featureide.fm.core.base.FeatureUtils;
@@ -18,7 +23,12 @@ import de.ovgu.featureide.fm.core.base.IFeatureModel;
  */
 public class NFProps {
 
-	public  static Object[] computeNFP(IFeatureProject featureProject, IFeature fc){
+	public static Set<String> names = new HashSet<>();
+	
+	public  static Object[] computeNFP(IFeatureProject featureProject, String featureCenter){
+		
+		IFeatureModel featureModel = featureProject.getFeatureModel();
+		IFeature fc = featureModel.getFeature(featureCenter);
 		List<String> nfp = new ArrayList<String>();
 		List<String> NFPValues = new ArrayList<String>();
 		int quantityCount = 0;
@@ -34,6 +44,7 @@ public class NFProps {
 					quantityCount++;
 					if(keyWithMinMax.containsKey(st[0])){
 						NFPValues.add(normalize(Double.parseDouble(st[1]), keyWithMinMax.get(st[0])));
+					
 					}
 				}else{
 					NFPValues.add(getEquivalentNumber(st[1]));
@@ -45,6 +56,64 @@ public class NFProps {
 		return new Object[]{nfp, NFPValues,quantityCount};
 	  }	
 	
+	public static List<Double> NFPGraphFeatures(IFeatureProject featureProject, String featureCenter,String nfpCenter) throws CoreException{
+	   
+	   HashMap<String, List<Double>> nfPropsHash = new HashMap<>();
+	   
+	   List<Double> valueList = null;
+	   Object[] featurePredicts = ConfigAnalysisUtils.computeFeaturePredictions(featureProject,featureCenter);
+	   List<String> relatedFeatures = (List<String>)featurePredicts[0];
+	   List<Double> featuresNFPValues = new ArrayList<>();
+	   List<String> featuresNFP = new ArrayList<>();
+	   System.out.println(relatedFeatures);
+	   for(String f : relatedFeatures){
+		   Object[] nfProperties = computeNFP(featureProject,f);
+		   List<String> nfp = (List<String>)nfProperties[0]; 
+		   for(int i = 0 ; i < nfp.size() ; i ++){
+			   if(nfp.get(i) == null || nfp.get(i).trim().length()== 0 || nfp.get(i).trim().equalsIgnoreCase("")){
+				   nfp.remove(i);
+				   i--;
+			   }
+				   
+		   } 
+		   List<String> nfpValues = (List<String>)nfProperties[1];
+		   
+		   for(String s :  names){
+			   if(!nfp.contains(s)){
+				   nfp.add(s);
+				   nfpValues.add("2.0");
+			   }
+		   }
+		   System.out.println("");
+		   
+		   try{
+				 for(int i=0; i< nfp.size();i++){
+					   if(nfPropsHash.containsKey(nfp.get(i))) {
+						   nfPropsHash.get(nfp.get(i)).add(Double.parseDouble(nfpValues.get(i)));
+						}else{
+							valueList = new ArrayList<>();
+							valueList.add(Double.parseDouble(nfpValues.get(i)));
+							nfPropsHash.put(nfp.get(i), valueList);
+						}
+				   }
+		   } catch(Exception ex){
+			   ex.printStackTrace();
+		   }
+	   }
+	   
+	   for(String key : nfPropsHash.keySet()){
+		   featuresNFP.add(key);
+		   if(key.equals(nfpCenter)){
+			   featuresNFPValues.addAll(nfPropsHash.get(key));
+		   }
+		   System.out.println(nfPropsHash.get(key));
+	   }
+	
+	 
+	  System.out.println("featuresNFPValues ..."+featuresNFPValues);
+	  System.out.println("featuresNFP.."+featuresNFP);
+		return featuresNFPValues;
+	}
 	
 	private static String getEquivalentNumber(String b){
 		String result = null;
@@ -70,6 +139,8 @@ public class NFProps {
 		if(minMax.length == 2 
 				&& (Double.parseDouble( minMax[0]) != Double.parseDouble( minMax[1]))){
 				result = 	(actual - Double.parseDouble(minMax[0]))/((Double.parseDouble(minMax[1]) - Double.parseDouble(minMax[0])));
+		}else{
+			result = 0.5;
 		}
 		return result+"";
 		
@@ -79,6 +150,8 @@ public class NFProps {
 	{
 	  return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
 	}
+	
+	
 	
 	public static HashMap<String, String> computeQuantity(IFeatureProject featureProject) {
 		IFeatureModel featureModel = featureProject.getFeatureModel();
@@ -91,7 +164,11 @@ public class NFProps {
 				String[] eachLine = FeatureUtils.getDescription(f).split("\\r?\\n");
 				for (String a : eachLine) {
 					String[] keyValStr = a.split(":");
+					if(keyValStr.length==2){
+						names.add(keyValStr[0]);
+					}
 					if(keyValStr.length==2 && isNumeric(keyValStr[1])){
+						
 						if(NFPHash.containsKey(keyValStr[0])) {
 							NFPHash.get(keyValStr[0]).add(Double.parseDouble(keyValStr[1]));
 						}
@@ -110,6 +187,7 @@ public class NFProps {
 			Collections.sort(NFPHash.get(key));
 			keyWithMinMax.put(key, NFPHash.get(key).get(0)+"-"+(NFPHash.get(key).size()==1 ?  NFPHash.get(key).get(0) : NFPHash.get(key).get(NFPHash.get(key).size()-1)));
 		}
+		System.out.println(keyWithMinMax);
 		return keyWithMinMax;
 	  }		
 }
